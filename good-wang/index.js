e92129850e9757a629cec910aa668536b14a70de
wangbingyi <1041493283@qq.com>
2022-06-04 07:57:59 +0800

    update

---
+++ good-wang/index.js
@@ -0,0 +1,18 @@
+// 一些常用的工具包
+const _ = require('lodash');
+const R = require('ramda');
+const axios = require('axios');
+
+function test() {
+  console.log('啦啦啦，哒哒哒')
+}
+
+module.exports = {
+  _, // Lodash.js
+  R, // Ramda.js
+  axios, // axios.js
+  test,
+}
+
+
+
---
+++ good-yang/index.js
@@ -0,0 +1,9 @@
+
+export function test() {
+  console.log('hello world')
+}
+
+
+
+
+
